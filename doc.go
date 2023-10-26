6b79b2fe2b3b3304810f14795663a2bfc16438a4
nicolito128 <nicolito128@hotmail.com>
2023-10-26 10:40:19 -0300

    update doc.go

--- doc.go
+++ doc.go
@@ -6,11 +6,13 @@ You can write the following example of hello world to test:
 	func main() {
 		router := plugo.New()
 
-		router.Get("/", func(conn plugo.Connection) error {
-			return conn.String(http.StatusOK, "Hello, Plugo World!")
+		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			conn := plugo.NewConnection(w, r)
+
+			conn.String(http.StatusOK, "Hello, Plugo World!")
 		})
 
-		fmt.Println("Server running at http://localhost:8080 - Press CTRL+C to exit")
+		fmt.Println("Server running at http://localhost:8080/ - Press CTRL+C to exit")
 		log.Fatal(http.ListenAndServe(":8080", router))
 	}
 */
